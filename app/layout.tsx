c1c82e81c4ee413cc1e7124524600fb0db55bd6e
Shreya Anand <Shryl13050@gmail.com>
2025-02-02 04:54:20 +0530

    third commit

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,20 +1,12 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+
 import "./globals.css";
 import { ClerkProvider } from "@clerk/nextjs";
 import Provider from "@/app/Provider"
 import { Toaster } from "sonner";
 // import LoadScript from "@/components/LoadScript";
 import { VoiceProvider } from '@/components/VoiceContext'
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
 
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -31,7 +23,7 @@ export default function RootLayout({
       
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        
       > 
         <Provider>
         {/* <LoadScript /> */}
